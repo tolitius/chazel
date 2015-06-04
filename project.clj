a8ba807118e85f6203a5f6404406fb97584c1b77
anatoly <tolitius@gmail.com>
2015-06-03 21:54:25 -0400

    releasing 0.1.1 with org.hface/hface-client

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chazel "0.1.1-SNAPSHOT"
+(defproject chazel "0.1.1"
   :description "hazelcast bells and whistles under the clojure belt"
   :url "https://github.com/tolitius/chazel"
   :license {:name "Eclipse Public License"
@@ -9,8 +9,9 @@
   :profiles {:dev {:repl-options {:init-ns chazel}}}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cprop "0.1.1-SNAPSHOT"]
+                 [cprop "0.1.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.hazelcast/hazelcast "3.4.2"]
                  [com.hazelcast/hazelcast-client "3.4.2"]
+                 [org.hface/hface-client "0.1.0"]
                  [clj-wallhack "1.0.1"]])
