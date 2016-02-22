f2f77bafa28bbb5e71ff6bca355f203b73ce6e33
anatoly <tolitius@gmail.com>
2016-02-22 09:20:12 -0500

    #2 upping HZ to 3.6 (and clj to 1.8)

--- project.clj
+++ project.clj
@@ -8,11 +8,11 @@
 
   :profiles {:dev {:repl-options {:init-ns chazel}}}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [cprop "0.1.1"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [com.hazelcast/hazelcast "3.5.3"]
-                 [com.hazelcast/hazelcast-client "3.5.3"]
+                 [com.hazelcast/hazelcast "3.6"]
+                 [com.hazelcast/hazelcast-client "3.6"]
                  [org.hface/hface-client "0.1.2"]
                  [com.cognitect/transit-clj "0.8.275"]
                  [clj-wallhack "1.0.1"]])
